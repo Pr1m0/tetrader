0b5b99c4e765348e9c57c8e69268a0467ece6b31
Elydra <kenyer.zsiros01@gmail.com>
2024-12-03 18:29:01 +0100

    Kész

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+
+  area!:number;
+  height!:number;
+  volume!:number;
+
+  startCalc(){
+
+    console.log("Számítás indul..."); 
+    this.volume=this.area*this.height / 3;
+  }
+}
+
+
